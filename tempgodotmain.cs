ed1b4622e9b6737f59bcbf950284e84009d5844b
snikolaj <51296436+snikolaj@users.noreply.github.com>
2023-03-22 21:10:23 +0100

    Create tempgodotmain.cs

---
+++ tempgodotmain.cs
@@ -0,0 +1,50 @@
+using Godot;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+public partial class MainCode : Sprite3D
+{
+	static SerialPort _serialPort;
+	byte[] writeArr = {(byte)'r', 0x19, 0x89};
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_serialPort = new SerialPort("COM8", 9600);
+		_serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+		_serialPort.Open();
+		
+		
+		if(_serialPort.IsOpen){
+			GD.Print("Open");
+		}
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+	
+	private static void DataReceivedHandler(
+						object sender,
+						SerialDataReceivedEventArgs e)
+	{
+		SerialPort sp = (SerialPort)sender;
+		string indata = sp.ReadExisting();
+		GD.Print("Data Received:");
+		GD.Print(indata);
+	}
+	private void _on_button_button_down()
+	{
+		_serialPort.Write(writeArr, 0, 3);
+		writeArr[1] += 1;
+	}
+
+}
+
+// data from https://learn.microsoft.com/en-us/dotnet/api/system.io.ports.serialport?view=dotnet-plat-ext-6.0
+
+
