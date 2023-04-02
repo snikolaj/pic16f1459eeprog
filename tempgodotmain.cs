d3078650b884ffbe917c14172b07a116d2375318
snikolaj <51296436+snikolaj@users.noreply.github.com>
2023-04-02 18:28:51 +0200

    Update tempgodotmain.cs

--- tempgodotmain.cs
+++ tempgodotmain.cs
@@ -2,16 +2,17 @@ using Godot;
 using System;
 using System.IO.Ports;
 using System.Threading;
+using System.Numerics;
 
 public partial class MainCode : Sprite3D
 {
 	static SerialPort _serialPort;
-	byte[] writeArr = {(byte)'r', 0x19, 0x89};
+	byte[] writeArr = {(byte)'r', 0x00, 0x00};
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_serialPort = new SerialPort("COM8", 9600);
+		_serialPort = new SerialPort("COM5", 9600);
 		_serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 		_serialPort.Open();
 		
@@ -36,11 +37,12 @@ public partial class MainCode : Sprite3D
 		string indata = sp.ReadExisting();
 		GD.Print("Data Received:");
 		GD.Print(indata);
+		
 	}
 	private void _on_button_button_down()
 	{
 		_serialPort.Write(writeArr, 0, 3);
-		writeArr[1] += 1;
+		writeArr[2] += 1;
 	}
 
 }
